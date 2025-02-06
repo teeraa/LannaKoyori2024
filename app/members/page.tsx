7e40bf015a4cf94154e090ac8f15705c5b3b52d1
givemeSomeBuck <105963462+teeraa@users.noreply.github.com>
2025-02-07 00:40:20 +0700

    old image path

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -415,7 +415,7 @@ export default function MembersList() {
                                                 ID={member.ID}
                                                 NameThai={member.NameThai || member.NameEng || "-"}
                                                 Role={member.RoleThai || "-"}
-                                                img={`/images/Koyori_${member.businessinfo?.DataYear}/${member.picture}` || ""}
+                                                img={`/images/entreprenuer/Koyori_${member.businessinfo?.DataYear}/${member.businessinfo?.ID}/Profile/${member.picture}` || ""}
                                                 Gender={member.gender || "-"}
                                             />
                                         ))
