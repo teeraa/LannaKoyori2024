01b8c8bb58ddd99c51fdb9b73aa852b74d86b635
chayakorn phukhiao <chayakorn.code@gmail.com>
2025-05-31 23:31:14 +0700

    create loading state

--- app/components/MemberCard.tsx
+++ app/components/MemberCard.tsx
@@ -23,30 +23,26 @@ export default function MemberCard({ NameThai, Role, img, ID }: MemberCardProps)
   return (
     <div className="bg-white rounded-md hover:shadow-lg p-4 cursor-pointer shadow-md w-full" onClick={handleMemberLink}>
       <div className="rounded-md bg-white w-full">
-        <div className="mx-auto w-44 h-44 group overflow-hidden">
+        <div className="mx-auto w-full aspect-square max-w-[200px] group overflow-hidden">
           <Image
             src={img || "/images/default.jpg"}
             alt={NameThai}
-            width={176}
-            height={176}
+            width={200}
+            height={200}
             quality={90}
             placeholder="blur"
             blurDataURL="/images/default.jpg"
             priority={true}
-            className="rounded-md transition-transform duration-300 group-hover:scale-110 origin-center"
-            style={{
-              objectFit: "cover",
-              objectPosition: "center center",
-              width: "100%",
-              height: "100%",
-            }}
+            className="rounded-md transition-transform duration-300 group-hover:scale-110 origin-center w-full h-full object-cover"
+            sizes="(max-width: 640px) 150px, (max-width: 768px) 180px, (max-width: 1024px) 200px, 220px"
           />
         </div>
+
         <div className="mt-2 w-full">
           <h1 className="text-xl text-start text-gray-600 truncate w-full">
             {NameThai}
-            </h1>
-          <h1 className="text-md text-start text-gray-400 flex justify-start items-center gap-2 truncate w-full">
+          </h1>
+          <h1 className="text-md text-start text-gray-400 flex justify-start items-center gap-2 truncate w-full font-light">
             <span>
               <VscTools size={16} />
             </span>
@@ -57,3 +53,6 @@ export default function MemberCard({ NameThai, Role, img, ID }: MemberCardProps)
     </div>
   );
 }
+
+
+
