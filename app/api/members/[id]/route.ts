e1bce17c6d3a1da064f4ac7ed004a8f3d23916fe
chayakorn phukhiao <chayakon@spacetrax.co>
2025-06-06 20:49:00 +0700

    -

--- app/api/members/[id]/route.ts
+++ app/api/members/[id]/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+
+
 export async function GET(
   request: NextRequest,
   context: any // ใช้ context ที่มี params
